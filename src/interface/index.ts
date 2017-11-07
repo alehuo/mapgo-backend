24551180b1a6192f5a778b2fd6422b9f82b8e769
alehuo <aleksi.huotala@helsinki.fi>
2017-11-07 21:56:44 +0200

    Added an exporter class to make exporting interfaces prettier.

---
+++ src/interface/index.ts
@@ -0,0 +1,8 @@
+export {default as Comparable}
+from './Comparable';
+export {default as Comparator}
+from './Comparator';
+export {default as JsonEdge}
+from './JsonEdge';
+export {default as JsonNode}
+from './JsonNode';
