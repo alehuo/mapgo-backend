bbd4c0bffdbd38835b19208b7ffb7436e373cd39
alehuo <aleksi.huotala@helsinki.fi>
2017-11-07 22:29:33 +0200

    Remove unused imports

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,3 @@
-// src/app.ts
-
-import * as Utils from './utils';
-import JsonNode from './interface/JsonNode';
-import Dijkstra from './algo/Dijkstra';
-import {Edge, ArrayList, Tuple, Coordinate} from './struct';
-import * as express from 'express';
 import Server from './Server';
 
 // Opens a server at a predefined port. Loads a graph into memory and allows drawing maps using various path finding algorithms.
